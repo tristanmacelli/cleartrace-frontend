8a7725a2d894b3695fa0d9391dcd9843f5e7405a
Tree <tristanmacelli@gmail.com>
2021-01-27 17:13:12 -0800

    [ADD] Modal component for group actions and signing up

--- src/components/GroupUpdate.vue
+++ src/components/GroupUpdate.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="modal">
-    <div class="container">
-      <div class="modal__title">{{ UpdateMessage }}</div>
-      <p>{{ GroupName }} group has been {{ Action }}</p>
-      <a v-if="groupDeleted" href="#" class="btn go-to">
-        Go to
-        {{ GroupName }}
-      </a>
-      <button class="mt-3 border-b border-teal font-semibold">Close</button>
-    </div>
+    <p class="">{{ UpdateMessage }}</p>
+    <p>{{ GroupName }} group has been {{ Action }}</p>
+    <a v-if="groupDeleted" href="#" class="btn go-to">
+      Go to
+      {{ GroupName }}
+    </a>
+    <button class="mt-3 border-b border-teal font-semibold">Close</button>
   </div>
 </template>
 
@@ -40,9 +38,3 @@ export default {
   }
 };
 </script>
-
-<style lang="css" scoped>
-.modal {
-  background-color: rgba(0, 0, 0, 0.7);
-}
-</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="modal">
+    <div>
+      <p class="text-2xl font-bold">{{ Title }}</p>
+      <p>{{ Description }}</p>
+      <img />
+    </div>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "modal",
+  props: {
+    Title: {
+      type: String,
+      required: true
+    },
+    Description: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
