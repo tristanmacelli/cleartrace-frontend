6f6cc40d1aaacf54a6776bbef4f7d179a2b6a297
Tree <tristanmacelli@gmail.com>
2021-03-19 13:55:55 -0700

    [ADD] Error and Warning handler functions to Vue app

--- src/main.js
+++ src/main.js
@@ -6,6 +6,18 @@ import "./index.css";
 
 const app = createApp(App);
 
+app.config.errorHandler = (err, vm) => {
+  console.log(err, vm);
+  // handle error
+  // `info` is a Vue-specific error info, e.g. which lifecycle hook
+  // the error was found in
+};
+
+app.config.warnHandler = function(msg, vm, trace) {
+  console.log(msg, vm, trace);
+  // `trace` is the component hierarchy trace
+};
+
 app.use(router);
 app.use(store);
 app.mount("#app");
