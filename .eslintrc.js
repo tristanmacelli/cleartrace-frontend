db2efd04fd86039331bf9baa7c866832c550eef6
Tristan Macelli <tristanmacelli@gmail.com>
2023-02-16 15:27:47 -0600

    [CHANGE] update all packages to latest possible

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,24 @@
 module.exports = {
   root: true,
+
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "@babel/eslint-parser",
+    exclude: ["node_modules"],
   },
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/prettier",
+    "plugin:prettier/recommended",
+  ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {},
   },
   variants: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 };
