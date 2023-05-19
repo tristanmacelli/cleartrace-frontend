496c2a39f3d72e88fd337939c6833bc641ba0aee
Tristan Macelli <tristanmacelli@gmail.com>
2023-05-19 00:37:57 -0500

    [UPDATE] eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
     node: true,
   },
 
+  parser: "vue-eslint-parser",
+
   parserOptions: {
     parser: "@typescript-eslint/parser",
     exclude: ["node_modules"],
@@ -14,6 +16,8 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
+
+  plugins: ["prettier", "@typescript-eslint"],
 
   extends: [
     "plugin:vue/vue3-essential",
