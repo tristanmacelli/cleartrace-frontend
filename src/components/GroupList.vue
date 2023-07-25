ada36ce5678c157ff5fef9578b7e6c33e4db100a
Tristan Macelli <tristanmacelli@gmail.com>
2023-07-25 18:06:03 -0500

    [FIX] remove group list from the DOM when hidden on mobile devices

--- src/components/GroupList.vue
+++ src/components/GroupList.vue
@@ -1,50 +1,52 @@
 <template>
   <!-- https://dev.to/fayaz/making-a-navigation-drawer-sliding-sidebar-with-tailwindcss-blueprint-581l -->
-  <div
-    id="groupList"
-    class="absolute sm:static sm:right-0 z-20 w-full sm:w-64 md:w-96 h-screen border-l border-gray-300 bg-white transition-all duration-300"
-    :class="isGroupListOpen ? '' : 'translate-x-full overflow-hidden'"
-  >
-    <!-- TheRightMenu.vue -->
-    <div class="flex no-wrap items-center px-2 h-20">
-      <h3 class="flex-grow text-2xl ml-3">Conversations</h3>
-      <button
-        class="h-8 mx-1.5 rounded-3xl bg-gray-200 hover:bg-gray-300 focus:outline-none"
-        @click="DisplayModalCreate"
-      >
-        <img
-          src="../assets/plus.svg"
-          class="p-2 overflow-hidden"
-          width="32"
-          height="32"
-          alt="A plus icon"
-        />
-      </button>
-      <Dropdown>
-        <list
-          @active-list-item="HandleListItem"
-          :positionRight="true"
-          :items="listItems"
-        ></list>
-      </Dropdown>
+  <Transition>
+    <div
+      v-if="isGroupListOpen"
+      id="groupList"
+      class="absolute sm:static sm:right-0 z-20 w-full sm:w-64 md:w-96 h-screen border-l border-gray-300 bg-white"
+    >
+      <!-- TheRightMenu.vue -->
+      <div class="flex no-wrap items-center px-2 h-20">
+        <h3 class="flex-grow text-2xl ml-3">Conversations</h3>
+        <button
+          class="h-8 mx-1.5 rounded-3xl bg-gray-200 hover:bg-gray-300 focus:outline-none"
+          @click="DisplayModalCreate"
+        >
+          <img
+            src="../assets/plus.svg"
+            class="p-2 overflow-hidden"
+            width="32"
+            height="32"
+            alt="A plus icon"
+          />
+        </button>
+        <Dropdown>
+          <list
+            @active-list-item="HandleListItem"
+            :positionRight="true"
+            :items="listItems"
+          ></list>
+        </Dropdown>
+      </div>
+      <!-- TheGroupList.vue -->
+      <div class="px-2 divide-y">
+        <group
+          @click="CloseGroupList"
+          v-for="group in sortedGroupList"
+          :key="group.index"
+          :id="group.id"
+          :name="group.name"
+          :description="group.description"
+          :private="group.private"
+          :members="group.members"
+          :creator="group.creator"
+          :index="group.index"
+          :createdAt="group.createdAt"
+        ></group>
+      </div>
     </div>
-    <!-- TheGroupList.vue -->
-    <div class="px-2 divide-y">
-      <group
-        @click="CloseGroupList"
-        v-for="group in sortedGroupList"
-        :key="group.index"
-        :id="group.id"
-        :name="group.name"
-        :description="group.description"
-        :private="group.private"
-        :members="group.members"
-        :creator="group.creator"
-        :index="group.index"
-        :createdAt="group.createdAt"
-      ></group>
-    </div>
-  </div>
+  </Transition>
 </template>
 
 <script lang="ts">
@@ -106,3 +108,15 @@ const HandleListItem = async (item: ListItem) => {
   }
 };
 </script>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.3s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  transform: translateX(100%);
+}
+</style>
