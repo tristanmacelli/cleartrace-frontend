ef49eae1f0097f8b97e3736c22f6b519ee506e06
Tristan Macelli <tristanmacelli@gmail.com>
2023-08-10 13:39:42 -0500

    [FIX] Flash of unstyled content (FOUC)

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,11 +9,6 @@ module.exports = {
     exclude: ["node_modules"],
   },
 
-  // rules: {
-  //   "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-  //   "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  // },
-
   plugins: ["prettier", "@typescript-eslint"],
 
   extends: [
